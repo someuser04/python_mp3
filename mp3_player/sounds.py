135766944afafb0ae1f7f3a18cb9809c3b8836a4
Juan Alberto Salgado <115202279+someuser04@users.noreply.github.com>
2023-04-20 02:52:20 -0500

    Update sounds.py
    working on fixing the buttons so that they work, issue is not the code but the images and directory stuff

--- mp3_player/sounds.py
+++ mp3_player/sounds.py
@@ -1,5 +1,6 @@
 from tkinter import *
 import pygame
+from tkinter import filedialog
 root = Tk()
 root.title('Mp3 player')
 root.iconbitmap()
@@ -7,28 +8,50 @@ root.geometry("500x400")
 
 pygame.mixer.init()
 
-song_box = Listbox(root, bg="black", fg="green", width=60)
-song_box.pack(pady=20)
+def add_track():
+    song = filedialog.askopenfile(initialdir='audio/', title="Choose a song", filetypes=(("mp3 Files", "*.mp3"), ))
+    #song = song.replace("C:/Users/juant/Desktop/python_mp3/mp3_player/audio/", "")
+    #song = song.replace(".mp3", "")
+    song_list.insert(END, song)
+def play():
+    song = song_list.get(ACTIVE)
+    song = f'audio/{song}.mp3'
 
-back_btn_img = PhotoImage(file='back_button.png')
-foward_btn_img = PhotoImage(file='foward_button.png')
-play_btn_img = PhotoImage(file='play_button.jpg')
-pause_btn_img = PhotoImage(file='pause_button.png')
-stop_btn_img = PhotoImage(file='stop_button.png')
+    pygame.mixer.music.load(song)
+    pygame.mixer.music.play(loops=0)
+def stop():
+    pygame.mixer.music.stop()
+    song_list.selection_clear(ACTIVE)
+
+song_list = Listbox(root, bg="black", fg="green", width=60, selectbackground="gray", selectforeground="black")
+song_list.pack(pady=20)
+
+#back_button = PhotoImage(file='button_back50.png')
+#foward_button = PhotoImage(file='mp3_player/photos/button_forward50.png')
+#play_button = PhotoImage(file='mp3_player/photos/play50.png')
+#pause_button = PhotoImage(file='mp3_player/photos/pause50.png')
+#stop_button = PhotoImage(file='mp3_player/photos/stop50.png')
+
 
 controls_frame = Frame(root)
 controls_frame.pack()
 
-back_button = Button(controls_frame, image=back_btn_img, borderwidth=0)
-foward_button = Button(controls_frame, image=foward_btn_img, borderwidth=0)
-play_button = Button(controls_frame, image=play_btn_img, borderwidth=0)
-pause_buton = Button(controls_frame, image=pause_btn_img, borderwidth=0)
-stop_button = Button(controls_frame, image=stop_btn_img, borderwidth=0)
+#back_button = Button(controls_frame, image=back_button, borderwidth=0)
+#foward_button = Button(controls_frame, image=foward_button, borderwidth=0)
+#play_button = Button(controls_frame, image=play_button, borderwidth=0)
+#pause_button = Button(controls_frame, image=pause_button, borderwidth=0)
+#stop_button = Button(controls_frame, image=stop_button, borderwidth=0)
 
-back_button.grid(row=0, column=0)
-foward_button.grid(row=0, column=1)
-play_button.grid(row=0, column=2)
-pause_buton.grid(row=0, column=3)
-stop_button.grid(row=0, column=4)
+#back_button.grid(row=0, column=0, padx=10)
+#foward_button.grid(row=0, column=1, padx=10)
+#play_button.grid(row=0, column=2, padx=10)
+#pause_button.grid(row=0, column=3, padx=10)
+#stop_button.grid(row=0, column=4, padx=10)
 
-root.mainloop() 
+menu = Menu(root)
+root.config(menu=menu)
+
+add_song_menu = Menu(menu)
+menu.add_cascade(label="Add Songs", menu=add_song_menu)
+add_song_menu.add_command(label="Add song to playlist", command=add_track)
+root.mainloop()
