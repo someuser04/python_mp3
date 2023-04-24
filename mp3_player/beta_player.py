2ece2d0e8cf76a40cf3e97150a9c579994a21866
Karen <112573757+kaaeekool@users.noreply.github.com>
2023-04-24 23:28:12 +0000

    background function and button

--- mp3_player/beta_player.py
+++ mp3_player/beta_player.py
@@ -1,5 +1,9 @@
+import tkinter
 from tkinter import *
 import pygame
+from tkinter.filedialog import askopenfilename
+from PIL import Image, ImageTk
+
 root = Tk()
 root.title('Mp3 player')
 root.iconbitmap()
@@ -11,11 +15,27 @@ def play():
     pygame.mixer.music.play(loops=0)
 def stop():
     pygame.mixer.music.stop()
+#add background image
+def add_pic():
+    file = askopenfilename()
+    back_pic = ImageTk,PhotoImage(Image.open(file))
+
+    plabel = tkinter.Label(image = back_pic)
+    plabel.configure(image=back_pic)
+    plabel.image = back_pic
+
+    pic_label = Label(root, image = back_pic, height = 450, width = 500)
+    pic_label.place(x = 0, y = 0)
+    pic_label.lower()
+    root.attributes("-transparent")
 
 mybutton = Button(root, text = "Play Song", font=("Comic Sans MS", 32), command = play)
 mybutton.pack(pady=20)
 stop_button = Button(root, text="Stop", command=stop)
 stop_button.pack(pady=20)
+#backgroung image button
+bButton = Button(root, text = "Background", command = add_pic)
+bButton.pack(pady = 20)
 
 root.mainloop()
 #root.blit(background_image, (0, 0))
