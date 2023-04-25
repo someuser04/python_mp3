b7521027a9d2e947418802564b5f199fb67507a0
Juan Alberto Salgado <115202279+someuser04@users.noreply.github.com>
2023-04-24 22:11:39 -0500

    Update sounds.py

--- mp3_player/sounds.py
+++ mp3_player/sounds.py
@@ -4,42 +4,50 @@ from tkinter import filedialog
 import time
 from mutagen.mp3 import MP3
 import tkinter.ttk as tkk
-root = Tk()
-root.title('Mp3 player')
+import os
+from pathlib import Path
+import random
+root = Tk(os.path.abspath('python_mp3'))     
+root.title('Python Player')
 root.iconbitmap()
 root.geometry("500x450")
-
+rgb = ["#" +''.join([random.choice('ABCDEF0123456789') for i in range(6)])]
+root.configure(background=rgb)
 pygame.mixer.init()
 
-def add_track():
-    song = filedialog.askopenfile(initialdir='audio/', title="Choose a song", filetypes=(("mp3 Files", "*.mp3"), ))
-    #song = song.replace("C:/Users/juant/Desktop/python_mp3/mp3_player/audio/", "")
-    #song = song.replace(".mp3", "")
-    song_list.insert(END, song)
 def add_tracks():
-    songs = filedialog.askopenfile(initialdir='audio/', title="Choose a song", filetypes=(("mp3 Files", "*.mp3"), ))
+    songs = filedialog.askopenfilenames(initialdir='audio', title="Choose a song", filetypes=(("mp3 Files", "*.mp3"), ))
 
-    for song in songs:
-        #song = song.replace("C:/Users/juant/Desktop/python_mp3/mp3_player/audio/", "")
-        #song = song.replace(".mp3", "")
+    for song_file in songs:
+        song = song_file.replace("C:/Users/juant/Desktop/python_mp3/audio/", "")
+        song = song.replace(".mp3", "")
         song_list.insert(END, song)
+
 def play():
     song = song_list.get(ACTIVE)
     song = f'audio/{song}.mp3'
 
     pygame.mixer.music.load(song)
     pygame.mixer.music.play(loops=0)
-    time()
-
-    #slider_position = int(song_l)
-    #slider.config(to=slider_position, value=0)
+   
 
 def stop():
     pygame.mixer.music.stop()
     song_list.selection_clear(ACTIVE)
 
     status.config(text='')
-
+global paused
+paused = False
+def pause(is_paused):
+    global paused
+    paused = is_paused
+    if paused == True:
+        pygame.mixer.music.unpause()
+        paused = False
+    else:
+        pygame.mixer.music.pause()
+        paused = True
+        
 def foward_song():
     next_song = song_list.curselection()
     next_song = next_song[0] + 1 
@@ -79,77 +87,43 @@ def clear_track():
     song_list.delete(0,END)
     pygame.mixer.music.stop()
 
-def time():
-    current_time = pygame.mixer.music.get_pos() / 1000
-    slider_label.config(text=f'Slider: {int(slider.get())} and Song Pos: {int(current_time)}')
-    converted_current_time = time.strftime('%H:%M:%S', time.gmtime(current_time))
-    status.config(text=f'Time Elapsed:{converted_current_time} of {converted_song_l}  ')
-    slider.config(value=int(current_time))
-    #current_song = song_list.curselection()
-    song = song_list.get(ACTIVE)
-    song = f'audio/{song}.mp3'
 
-    song_mute = MP3(song)
-    global song_l
 
-    song_l = song_mute.info.length
-    converted_song_l = time.strftime('%H:%M:%S', time.gmtime(song_l))
-    current_time += 1
-    if int(slider.get()) == int(current_time):
-        # slider hasnt been moved
-        slider_position = int(song_l)
-        slider.config(to=slider_position, value=int(current_time))
-    else:
-        slider_position = int(song_l)
-        slider.config(to=slider_position, value=int(slider.get()))
-        converted_current_time = time.strftime('%H:%M:%S', time.gmtime(current_time))
-        status.config(text=f'Time Elapsed:{converted_current_time} of {converted_song_l}  ')
+# Create Volume Function
+def volume(x):
+     pygame.mixer.music.set_volume(1-volume_slider.get())
+     current_volume = pygame.mixer.music.get_volume()
+     slider_label.config(text=int((current_volume)*100))
 
-    slider_position = int(song_l)
-    
-    slider.config(to=slider_position, value=int(current_time))
-    
-    status.after(1000, time)
-global paused
-paused = False
+# Create Master Frame
+master_frame = Frame(root, bg="gray")
+master_frame.pack(pady=20, padx=(30,0))
 
-def pause(is_paused):
-    global paused
-    paused = is_paused
+song_list = Listbox(master_frame, bg = "gray", fg="white", width=60, selectbackground="white", selectforeground="black")
+song_list.grid(row=1, column=0, columnspan = 2)
 
-    if paused:
-        pygame.mixer.music.unpause()
-        paused = False
-    else:
-        pygame.mixer.music.pause()
-        paused = True
 
-def slide(x):
-    #slider_label.config(text=f'{int(slider.get())} of {int(song_l)}')
-    song = song_list.get(ACTIVE)
-    song = f'audio/{song}.mp3'
 
-    pygame.mixer.music.load(song)
-    pygame.mixer.music.play(loops=0, start=int(slider.get()))
-
-song_list = Listbox(root, bg="black", fg="green", width=60, selectbackground="gray", selectforeground="black")
-song_list.pack(pady=20)
-
-back_button = PhotoImage(file='C:/Users/juant/Desktop/python_mp3/mp3_player/photos/button_back50.png')
-foward_button = PhotoImage(file='C:/Users/juant/Desktop/python_mp3/mp3_player/photos/button_forward50.png')
-play_button = PhotoImage(file='C:/Users/juant/Desktop/python_mp3/mp3_player/photos/play50.png')
-pause_button = PhotoImage(file='C:/Users/juant/Desktop/python_mp3/mp3_player/photos/pause50.png')
-stop_button = PhotoImage(file='C:/Users/juant/Desktop/python_mp3/mp3_player/photos/stop50.png')
+back_button_img = PhotoImage(file=('static/11.png'))
+foward_button_img = PhotoImage(file=('static/8.png'))
+play_button_img = PhotoImage(file=('static/7.png'))
+pause_button_img= PhotoImage(file=('static/10.png'))
+stop_button_img= PhotoImage(file=('static/6.png'))
 
 
 controls_frame = Frame(root)
 controls_frame.pack()
 
-back_button = Button(controls_frame, image=back_button, borderwidth=0)
-foward_button = Button(controls_frame, image=foward_button, borderwidth=0)
-play_button = Button(controls_frame, image=play_button, borderwidth=0)
-pause_button = Button(controls_frame, image=pause_button, borderwidth=0)
-stop_button = Button(controls_frame, image=stop_button, borderwidth=0)
+# Create Volume Label Frame
+volume_frame = LabelFrame(master_frame, text="Volume", bg="gray")
+volume_frame.grid(row=1, column=3, padx=(0,0))
+
+
+back_button = Button(controls_frame, image=back_button_img, borderwidth=0,command=prev_song)
+foward_button = Button(controls_frame, image=foward_button_img, borderwidth=0,command=foward_song)
+play_button = Button(controls_frame, image=play_button_img, borderwidth=0,command=play)
+pause_button = Button(controls_frame, image=pause_button_img, borderwidth=0,command=lambda: pause(paused))
+stop_button = Button(controls_frame, image=stop_button_img, borderwidth=0,command=stop)
 
 back_button.grid(row=0, column=0, padx=10)
 foward_button.grid(row=0, column=1, padx=10)
@@ -162,7 +136,6 @@ root.config(menu=menu)
 
 add_song_menu = Menu(menu)
 menu.add_cascade(label="Add Songs", menu=add_song_menu)
-add_song_menu.add_command(label="Add song to playlist", command=add_track)
 
 add_song_menu.add_command(label="Add songs to playlist", command=add_tracks)
 
@@ -171,13 +144,20 @@ menu.add_cascade(label="Remove Songs", menu=remove_song_menu)
 remove_song_menu.add_command(label="Delete a Song from playlist", command = delete_song)
 remove_song_menu.add_command(label="Clear Playlist", command = clear_track)
 
+# Create Volume Slider
+volume_slider = tkk.Scale(volume_frame, from_=0, to=1, orient=VERTICAL, value=0, command=volume, length = 111)
+volume_slider.pack(pady=(10, 4))
+
+# Create Current Volume Label
+slider_label = Label(volume_frame, text="100", bg="gray")
+slider_label.pack()
+
+
 status = Label(root, text='', bd=1, relief=GROOVE, anchor=E)
 status.pack(fil=X, side=BOTTOM, ipady=2)
 
-slider = tkk.Scale(root, from_=0, to=100, orient=HORIZONTAL, value=0, command=slide, length=360)
-slider.pack(pady=30)
 
-slider_label = Label(root, text="0")
-slider_label.pack(pady=10)
+
 
 root.mainloop()
+
